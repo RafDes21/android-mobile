579c0e64ff4c3027c407f4e6762c81268022b03f
israe <israeltineo84@gmail.com>
2024-07-20 21:10:36 -0300

    add progressTest

--- app/src/main/java/com/rafdev/androidmobile/main/MainActivity.kt
+++ app/src/main/java/com/rafdev/androidmobile/main/MainActivity.kt
@@ -4,12 +4,15 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
+import com.rafdev.androidmobile.R
 import com.rafdev.androidmobile.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,12 +20,40 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         askNotificationPermission()
+        testProgress()
+    }
+
+    private fun testProgress() {
+//        binding.progressHorizontalOne.progress = 30
+        binding.progressHorizontalTow.progress = 30
+        binding.contentOne.setOnClickListener {
+            binding.textOne.isSelected = true
+            binding.progressHorizontalOne.progressDrawable =
+                ContextCompat.getDrawable(this, R.drawable.custom_progress_inactive)
+
+        }
+        val runnable = object : Runnable {
+            var progress = 10
+            override fun run() {
+                if (progress < 100) {
+                    progress += 10
+                    binding.progressHorizontalOne.progress = progress
+                    handler.postDelayed(this, 2 * 1000)
+                } else {
+                    binding.progressHorizontalOne.progress = 0
+                    progress = 10
+                    handler.postDelayed(this, 2 * 1000)
+                }
+            }
+        }
+        handler.post(runnable)
     }
 
 
@@ -46,6 +77,11 @@ class MainActivity : AppCompatActivity() {
         } else {
 
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(null)
     }
 
 }
